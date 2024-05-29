4d9010853ea06c14d10571e9e0c1c6cd0b03b7e1
Brennan <blfische92@gmail.com>
2024-05-29 20:07:56 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace Challenge3._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter word to test if palidrome");
+            Console.WriteLine(isPalidrome(Console.ReadLine())); 
+
+
+            bool isPalidrome(string userInput)
+            {
+                for (int i = 0; i < userInput.Length / 2; i++)
+                {
+                    if (userInput[i] != userInput[userInput.Length - i - 1])
+                            {
+                        return false;
+                    }
+                }
+                return true; 
+            }
+        }
+    }
+}
